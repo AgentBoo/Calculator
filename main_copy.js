c225804a1cc530d53690ca12e24975d1923ce486
AgentBoo <zuzi.han@gmail.com>
2017-08-10 21:16:54 -0400

    renamed variables in .js

--- main_copy.js
+++ main_copy.js
@@ -1,222 +1,157 @@
-// NOTE:
-let num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let op = ["C", ".", "=", "+", "-", "x", "/"];
-
 // NOTE: Displaying actions
-// e.target == ref to object that dispatched the event
-// e.currentTarget
+// e.target == element that dispatched the event -- event handler attached to this
+// e.currentTarget == element on which event happend
 
 // NOTE: Test if Kirupa's method works
 // const queryKirupa = document.querySelector(".buttons");
 // queryKirupa.addEventListener("mouseover", hide, false);
 //
-// function hide(kitten){
-//   let win = document.getElementById("window");
-//   if (kitten.target !== kitten.currentTarget){
-//     kitten.target.style.visibility = "hidden";
-//   }
-//
-//   kitten.stopPropagation();
-// }
+//    function hide(kitten){
+//      let win = document.getElementById("window");
+//      if (kitten.target !== kitten.currentTarget){
+//        kitten.target.style.visibility = "hidden";
+//      } kitten.stopPropagation();
+//    }
 
-// NOTE: Eval method
+// NOTE: Eval() method
 //   var b = "3+2/-2";
 //   console.log(eval(b));
 
-// NOTE: Setup
-const calcButton = document.querySelector(".buttons");
-// const results = document.getElementById("results");
-const win = document.getElementById("window");
-let realresults = document.createElement("div");
-let sidepanel = document.getElementById("memory");
+// NOTE: Proof of concept
+// let f = 3, g = -4;
+// let t = f + g;
+// console.log(t);
+
+
+// NOTE: Variable declarations
+const calcButtons = document.querySelector(".calcButtons");
+const inputWin = document.getElementById("inputWindow");
+// const outputWin = document.getElementById(outputWindow);
+let outputLog = document.getElementById("outputLog"); // was previously sidePan
+// let outputWinRes = document.createElement("div"); // was previously realresults
+
+// "Hippokampus"
+// let a = []; //was previously a
+let f = []; // was previously b first array
+let gD = []; // was previously memry digits individually
+let mD= []; //was previously newarray multidigits
+let lingebraAr=[]; //was previously resultsArray
 
 
 
-//this is my single memory
-let memry = [];
-let a = [];
-let b = [];
-let newarray = [];
-let resultsArray=[];
 
-calcButton.addEventListener("click", toDisplay, false);
+calcButtons.addEventListener("click", toDisplay, false);
 
-// NOTE: proof of concepts
-let f = 3, g = -4;
-let t = f + g;
-console.log(t);
-
-
-// NOTE: Functions in use
-// display what was clicked in a separate teal window, except C and =
 function toDisplay(kitten){
-  resultsArray = [];
+  lingebraAr = [];
 
+  if (kitten.target !== kitten.currentTarget && kitten.target.id == "inputWindow"){
+    inputWin.append="";
 
-  if(kitten.target !== kitten.currentTarget && kitten.target.id == "clear"){
+} else if(kitten.target !== kitten.currentTarget && kitten.target.id == "clear"){
+    let p = document.createElement("p");
+        p.classList.add('outputLog-content');
+        p.append(`${inputWin.textContent}`);
+    outputLog.append(p);
 
-    let parSidepanel = document.createElement("p");
-    parSidepanel.classList.add('memoryEntries');
-    parSidepanel.append(`${win.textContent}`);
-    sidepanel.append(parSidepanel);
+    f = [];
+    gD = [];
+    mD = [];
+    lingebraAr=[];
+    inputWin.innerHTML="";
 
-    memry = [];
-    a = [];
-    b = [];
-    newarray = [];
-    resultsArray = [];
-
-    win.innerHTML="";
-
-  }
-
-  else if (kitten.target !== kitten.currentTarget && (kitten.target.id == "window" || kitten.target.id =="results")){
-
-      win.append="";
-  }
-
-  else if(kitten.target !== kitten.currentTarget && kitten.target.id !== "="){
+} else if (kitten.target !== kitten.currentTarget && kitten.target.id !== "="){
     //to display
-    win.append(kitten.target.id);
-    a.push(kitten.target.id);
-    b.push(kitten.target.id);
-    console.log(b);
+    inputWin.append(kitten.target.value);
 
-    // to brain
+    // to hippokampus
+    f.push(kitten.target.value);
     if (kitten.target.className == "op"){
-      let number = b.splice(0, b.length-1);
-      let operator = b.splice(0, 1);
-      memry.push(number);
-      memry.push(operator);
-      console.log(JSON.stringify(memry));
-
+      let number = f.splice(0, f.length-1);
+      let operator = f.splice(0, 1);
+      gD.push(number);
+      gD.push(operator);
+      // console.log(JSON.stringify(dI));
     }
+
   } else {
-
-    memry.push(b);
-    b =[];
-    console.log(b);
-    recalibrateArray(memry);
-
-
-
-    // realresults.innerHTML =
-    // `<div class="output" id="results">${newarray}</div>`
-    // calcButton.replaceChild(results, realresults);
-    }
+    gD.push(f);
+    recalibrateArray(gD);
   }
-
-
-// NOTE: Following lines are only for a specific case of array.length = 3 and addition
-function recalibrateArray(memry){
-  console.log(JSON.stringify(memry));
-  for (let i = 0; i < memry.length; i++){
-    let sum = [];
-
-// NOTE: concatenating strings
-    for (let j = 0; j < memry[i].length; j++){
-      sum += memry[i][j];
-    }
-    newarray.push(sum);
-  }
-  console.log(newarray);
-  recalibrateValues(newarray);
 }
 
 
-function recalibrateValues(newarray){
-  console.log(JSON.stringify(newarray));
+function recalibrateArray(gD){
+  for (let i = 0; i < gD.length; i++){
+
+// NOTE: concatenating array entries == strings
+    let cat = [];
+    for (let j = 0; j < gD[i].length; j++){
+      cat += gD[i][j];
+    } mD.push(cat);
+  } recalibrateValues(mD);
+}
+
+function recalibrateValues(mD){
+  console.log(JSON.stringify(mD));
 
   let multipArray = [];
   let multiplication = 0;
   let divArray = [];
   let division = 0;
-  let theRestArray = [];
 
-  for (let i = 0; i < newarray.length; i++){
-
-  if (newarray[i] == "x"){
-    multiplication = newarray[i-1]*newarray[i+1];
-    multipArray = newarray.splice(i-1,3,multiplication);
-    console.log(newarray);
-  }
-} takecareofdivision(newarray);
-  function takecareofdivision(newarray){
-    for (let i = 0; i < newarray.length;i++){
-      if (newarray[i] == "/"){
-        division = newarray[i-1]/newarray[i+1];
-        divArray = newarray.splice(i-1,3,division);
-        console.log(newarray);
-
-  }
+  for (let i = 0; i < mD.length; i++){
+    if (mD[i] == "x"){
+    multiplication = mD[i-1] * mD[i+1];
+    multipArray = mD.splice(i-1,3,multiplication);
     }
-  }
-
-  assignSign(newarray);
-
+  } takecareofdivision(mD);
 }
 
-function assignSign(newarray){
-  console.log(newarray);
+function takecareofdivision(mD){
+  for (let i = 0; i < mD.length;i++){
+    if (mD[i] == "/"){
+        division = mD[i-1]/mD[i+1];
+        divArray = mD.splice(i-1,3,division);
+    }
+  } assignSign(mD);
+}
+
+function assignSign(mD){
+  console.log(JSON.stringify(mD));
   let final=[];
   let calc = "";
-  for (let i = 0; i < newarray.length; i++){
-    if (newarray[i] == "+" || newarray[i] == "-"){
-      final = newarray.slice(i, i+2);
-      calc = final[0]+final[1];
-      resultsArray.push(calc);
-      console.log(resultsArray);
+  for (let i = 0; i < mD.length; i++){
+    if (mD[i] == "+" || mD[i] == "-"){
+      final = mD.slice(i, i+2);
+      calc = final[0] + final[1];
+      lingebraAr.push(calc);
     }
+  } lingebraAr.push(mD[0]);
+    summarize(lingebraAr);
+}
+
+function summarize(lingebraAr){
+  console.log(JSON.stringify(lingebraAr));
+  let resNum = 0;
+  for(let i = 0; i < lingebraAr.length;i++){
+  resNum += Number(lingebraAr[i]);
+  } if (resNum == Infinity){
+    console.log(resNum);
+    inputWin.innerHTML = "... pfff :D"
+    f=[];
+    gD = [];
+    mD=[];
+    lingebraAr=[];
+  } else {
+    console.log(resNum);
+    inputWin.innerHTML = resNum;
+    f=[];
+    gD = [];
+    mD = [];
+    lingebraAr=[];
+    f.push(resNum);
+    return resNum;
   }
-  console.log(newarray[0]);
-  resultsArray.push(newarray[0]);
-  // newarray = [];
-  console.log(resultsArray);
-  summarize(resultsArray);
+// NOTE: The very last bracket of the document
 }
-
-function summarize(resultsArray){
-  let whops = 0;
-
-  for(let i = 0; i < resultsArray.length;i++){
-  whops += Number(resultsArray[i]);
-  console.log(whops);
-  }
-
-  if (whops == Infinity){
-    win.innerHTML = "... pfff :D"
-  console.log(whops);
-  console.log(win);
-
-  a=[];
-  memry=[];
-  newarray=[];
-
-  resultsArray=[];
-
-
-} else {
-  win.innerHTML = whops;
-
-  a=[];
-  memry=[];
-  newarray=[];
-  resultsArray=[];
-  b.push(whops);
-  return whops;
-}
-}
-
-
-
-
-
-// }
-// function multpl(a,b){
-//   const s = a * b;
-//   return s;
-// }
-// function div(a,b){
-//   const s = a/b;
-//   return s;
-// }
